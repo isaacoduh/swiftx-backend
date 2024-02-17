05344bf8132b4a3bf3c749af737d3e4cf952f745
Isaac Oduh <isaacoduh@IOs-MacBook-Pro.local>
2024-02-17 04:11:32 +0000

    refactor

--- src/routes/order.routes.ts
+++ src/routes/order.routes.ts
@@ -2,7 +2,10 @@ import express from "express";
 import orderController from "../controllers/order.controller";
 
 const router = express.Router();
-router.post("/checkout/create-checkout-session");
+router.post(
+  "/checkout/create-checkout-session",
+  orderController.createCheckoutSession
+);
 router.post("/checkout/webhook", orderController.stripeWebhookHandler);
 
 export default router;
