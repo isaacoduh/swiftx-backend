1a883a97ec2524113d0ff2ad7aa81650ce9f2375
Isaac Oduh <isaacoduh@IOs-MacBook-Pro.local>
2024-02-17 02:32:43 +0000

    create store model

---
+++ src/models/store.model.ts
@@ -0,0 +1,24 @@
+import mongoose, { InferSchemaType } from "mongoose";
+const productItemSchema = new mongoose.Schema({
+  _id: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    default: () => new mongoose.Types.ObjectId(),
+  },
+  name: { type: String, required: true },
+  price: { type: Number, required: true },
+});
+
+export type ProductItemType = InferSchemaType<typeof productItemSchema>;
+
+const storeSchema = new mongoose.Schema({
+  shopName: { type: String, required: true },
+  city: { type: String, required: true },
+  country: { type: String, required: true },
+  productItems: [productItemSchema],
+  imageUrl: { type: String, required: true },
+  lastUpdated: { type: Date, required: true },
+});
+
+const Store = mongoose.model("Store", storeSchema);
+export default Store;
