e76d9ce5d4fbba7747d08d75d947163438fae3b5
Isaac Oduh <isaacoduh@IOs-MacBook-Pro.local>
2024-02-25 04:29:04 +0000

    add jwt session checkout session

--- src/routes/order.routes.ts
+++ src/routes/order.routes.ts
@@ -4,7 +4,12 @@ import orderController from "../controllers/order.controller";
 
 const router = express.Router();
 router.get("/", jwtCheck, jwtParse, orderController.getMyOrders);
-router.post("/create-checkout-session", orderController.createCheckoutSession);
+router.post(
+  "/create-checkout-session",
+  jwtCheck,
+  jwtParse,
+  orderController.createCheckoutSession
+);
 router.post("/handle-webhook", orderController.stripeWebhookHandler);
 
 export default router;
